af33e81bd68e44131c2fed5760115c02ecc690f1
Alexander Oronov <oron4e2@gmail.com>
2021-12-30 14:55:49 +0200

    Automation Project - lvl2 Homework

---
+++ AutomationProject001/AutomationProject001/AutomationProject001/LogInPage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+
+namespace AutomationProject001
+{
+    class LogInPage
+    {
+        IWebDriver _driver;
+        //        IWebDriver driver;
+
+        public LogInPage(IWebDriver webDriver)
+        {
+            _driver = webDriver;
+        }
+
+        /*
+                public LogInPage(IWebDriver driver)
+                {
+                    this.driver = driver;
+                }
+        */
+
+        private IWebElement UserNameField => _driver.FindElement(By.XPath("//input[@name='email']"));
+
+        private IWebElement PasswordField => _driver.FindElement(By.XPath("//input[@name='pass']"));
+
+        private IWebElement SubmitButton => _driver.FindElement(By.XPath("//button[@name='btn-login']"));
+
+        public void EnterUserName(string userName) 
+        {
+            UserNameField.Clear();
+            UserNameField.SendKeys(userName);
+        }
+        public void EnterPassword(string password)
+        {
+            PasswordField.Clear();
+            PasswordField.SendKeys(password);
+        }
+        public void ClickSubmitButton()
+        {
+            SubmitButton.Click();
+        }
+    }
+}
--- AutomationProject001/AutomationProject001/AutomationProject001/UnitTest1.cs
+++ AutomationProject001/AutomationProject001/AutomationProject001/UnitTest1.cs
@@ -1,6 +1,9 @@
+using System;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace AutomationProject001
 {
@@ -11,23 +14,30 @@ namespace AutomationProject001
         [SetUp]
         public void Setup()
         {
-            _driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            _driver = new ChromeDriver(options);
+
+            // bezusloven wait (implicit), za vseki edin element - namirane + rabota s nego, generalen wait
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
         }
 
         [Test]
         public void Test1()
         {
             _driver.Navigate().GoToUrl("http://localhost:8081/login.php");
-            _driver.Manage().Window.Maximize();
+            // _driver.Manage().Window.Maximize();
 
-            _driver.FindElement(By.XPath("//input[@name='email']")).Clear();
-            _driver.FindElement(By.XPath("//input[@name='email']")).SendKeys("admin@automation.com");
+            // explicit (usloven) wait, specific za stranica, element, moje da se preizpolzva ili da se iznese v base class, ima condition (napr. alert)
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
 
-            _driver.FindElement(By.XPath("//input[@name='pass']")).Clear();
-            _driver.FindElement(By.XPath("//input[@name='pass']")).SendKeys("pass123");
+            LogInPage logInPage = new LogInPage(_driver);
 
-            _driver.FindElement(By.XPath("//button[@name='btn-login']")).Click();
+            logInPage.EnterUserName("admin@automation.com");
+            logInPage.EnterPassword("pass123");
+            logInPage.ClickSubmitButton();
 
+            // Assert.IsFalse(_driver.FindElement(By.XPath("//p[@id='form.error']")).Displayed, "Invalid username/password!");
         }
 
         [TearDown]
